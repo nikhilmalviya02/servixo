96f2c523dbc8661f10e04d590f7457931f1b9620
nikhilmalviya02 <nikhilmalviya402@gmail.com>
2026-03-07 13:19:11 +0530

    change

--- config/db.js
+++ config/db.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected");
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("MongoDB Atlas Connected");
   } catch (error) {
-    console.log(error);
+    console.error(error);
     process.exit(1);
   }
 };
